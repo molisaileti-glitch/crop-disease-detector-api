bdf95f7bd24950738ff3c1de7b044c91ed53c032
molisaileti-glitch <molisaileti@gmail.com>
2026-05-16 20:15:08 +0300

    fixed python version for render deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Tell Render explicitly to use Python 3.11
+export PYTHON_VERSION=3.11.9
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run database migrations
 python manage.py migrate
